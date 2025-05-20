4c7614fd1ffa12f6904f4b32b707b6fe80e60ae5
etjen <buljubasicetjen4@gmail.com>
2025-05-20 21:45:24 +0200

    prompt

--- src/main/java/org/etjen/spring_ai/OpenAIController.java
+++ src/main/java/org/etjen/spring_ai/OpenAIController.java
@@ -2,13 +2,15 @@ package org.etjen.spring_ai;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+import java.util.Map;
+
+@RestController()
 public class OpenAIController {
     private final OpenAiChatModel openAiChatModel;
     private final ChatClient chatClient;
@@ -23,22 +25,44 @@ public class OpenAIController {
         this.chatClient = builder.build();
     }
 
-    @GetMapping("api/{message}")
+    @GetMapping("{message}")
     public String getAnswer(@PathVariable String message){
         return openAiChatModel.call("Provide simple one sentence answer/reply/whatever: " + message);
     }
 
-    @GetMapping("api/chat-client/{message}")
+    @GetMapping("chat-client/{message}")
     public ResponseEntity<String> getAnswerFromChatClient(@PathVariable String message){
         String response = chatClient.prompt(message).call().content();
         return ResponseEntity.ok("With chat client: " + response);
     }
 
-    @GetMapping("api/chat-response/{message}")
+    @GetMapping("chat-response/{message}")
     public ResponseEntity<String> getAnswerFromChatResponse(@PathVariable String message){
         ChatResponse chatResponse = chatClient.prompt(message).call().chatResponse();
         System.out.println(chatResponse.getMetadata().getModel());
         String response = chatResponse.getResult().getOutput().getText();
         return ResponseEntity.ok("With chat client: " + response);
+    }
+
+    @PostMapping("chat-template/movie-recommend")
+    public String recommendMovie(@RequestBody RecommendMoviePromptClass recommendMoviePromptClass){
+        String template = """
+                        I want to watch a {type} movie tonight with good rating,
+                        looking for movies around this year {year}.
+                        The language I'm looking for is {lang}.
+                        Suggest one specific movie and tell me the cast and length of the movie.
+                        
+                        Response format should be exactly like this:
+                        1. Movie name
+                        2. Basic plot
+                        3. Cast
+                        4. Length
+                        5. IMDB rating
+                        """;
+        PromptTemplate promptTemplate = new PromptTemplate(template);
+        Prompt prompt = promptTemplate.create(Map.of("type", recommendMoviePromptClass.getType(), "year", recommendMoviePromptClass.getYear(), "lang", recommendMoviePromptClass.getLang()));
+
+        String response = chatClient.prompt(prompt).call().content();
+        return response;
     }
 }
---
+++ src/main/java/org/etjen/spring_ai/RecommendMoviePromptClass.java
@@ -0,0 +1,31 @@
+package org.etjen.spring_ai;
+
+public class RecommendMoviePromptClass {
+    private String type;
+    private String year;
+    private String lang;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getLang() {
+        return lang;
+    }
+
+    public void setLang(String lang) {
+        this.lang = lang;
+    }
+}
